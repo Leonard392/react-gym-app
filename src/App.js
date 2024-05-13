5169c74ca4b9ec5f7908a1d6eecea2514a33090d
Leonardo Rodriguez <106454639+Leonard392@users.noreply.github.com>
2024-05-13 20:30:40 +0000

    router integrated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./front/views/home";
+import Navbar from "./front/components/navbar.jsx";
+import Footer from "./front/components/footer.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<h1>Not found!</h1>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/front/components/navbar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Navbar() {
+    return <h1>HELLO IM THE NAVBAR</h1>;
+}
