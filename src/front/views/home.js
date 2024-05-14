3a16dc3e71b8eb1f7358025de86ced37574ab790
Leonard392 <leorp392@gmail.com>
2024-05-14 14:34:07 -0400

    home started

--- src/front/views/home.js
+++ src/front/views/home.js
@@ -1,5 +1,20 @@
 import React from "react";
+import "../styles/home.css";
 
 export default function Home() {
-    return <h1>Welcome to the world of Geeks!</h1>;
+  return (
+    <div className="home-content">
+      <div className="main-home">
+        <h1>Welcome to</h1>
+        <h2>Brand Gym</h2>
+        <p>
+          asdasdasdkadmfdm fmds faakodsmfokads kofak nafko naf nkovnfdk a mfo
+          sdngolafnld klngg dfnnml nfldsm lndsf dfsm sk mbkl sgnfdlg
+          faakodsmfokads afdsn a dfnl nsdklklsngkldsnklfngk sndgnbkldgnkl
+          dflkgnfdklsgn lkn slkdflk dglsm .
+        </p>
+        <i class="fa-solid fa-dumbbell weight-icon"></i>
+      </div>
+    </div>
+  );
 }
