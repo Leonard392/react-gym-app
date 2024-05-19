8a859733e56ea05ec33d0723b087f9bb9035c8c1
Leonard392 <leorp392@gmail.com>
2024-05-19 15:39:15 -0400

    footer started

--- src/front/components/footer.jsx
+++ src/front/components/footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/footer.css";
 
 export default function Footer() {
-    return <h1>Hello Im the footer</h1>;
+  return <h1>aaaaaa</h1>;
 }
