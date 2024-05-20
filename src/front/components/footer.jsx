ec7e6aa2c4d4399368475eecfd76f90f1a87a8eb
Leonardo Rodriguez <106454639+Leonard392@users.noreply.github.com>
2024-05-20 18:17:41 +0000

    footer started

--- src/front/components/footer.jsx
+++ src/front/components/footer.jsx
@@ -3,5 +3,21 @@ import { Link } from "react-router-dom";
 import "../styles/footer.css";
 
 export default function Footer() {
-  return <h1>aaaaaa</h1>;
+  return (
+    <div className="footer">
+      <div className="contact-us">
+        <p><span>Email: </span>leorp392@yahoo.com</p>
+        <p><span>Phone: </span>954 901 6488</p>
+      </div>
+      <div className="code">
+        <a href="https://github.com/Leonard392/react-gym-app"><i class='bx bxl-github'></i></a>
+        <a href="https://www.linkedin.com/in/leonardorodriguez392/"><i class='bx bxl-linkedin-square'></i></a>
+      </div>
+      <div className="comming-soon">
+        <i class='bx bxl-facebook-circle' ></i>
+        <i class='bx bxl-instagram' ></i>
+        <i class='bx bxl-youtube' ></i>
+      </div>
+    </div>
+  )
 }
